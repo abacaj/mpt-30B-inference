d4fd75cd3aaa8c1c7bacd0b5b8355506fd96a0af
Anton Bacaj <abacaj1@gmail.com>
2023-06-26 17:23:01 +0000

    remove cache dir

--- download_model.py
+++ download_model.py
@@ -7,7 +7,8 @@ def download_mpt_quant(destination_folder: str, repo_id: str, model_filename: st
     return hf_hub_download(
         repo_id=repo_id,
         filename=model_filename,
-        cache_dir=local_path,
+        local_dir=local_path,
+        local_dir_use_symlinks=False
     )
 
 
